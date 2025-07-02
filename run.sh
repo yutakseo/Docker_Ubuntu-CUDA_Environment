191c1530db7d145caad889d0a18fcccaaeb0d9d0
Yutak Seo <yts@smartinside.ai>
2025-07-02 11:07:25 +0900

    25.07.02

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@
 # 설정값
 VOLUME_DIR="$(pwd)"
 DATASETS_FILE="___DATASETS___.list"
-DOCKER_IMAGE="ubuntu22.04_cuda11.08_image"
-CONTAINER_NAME="ubuntu22.04_cuda11.08_container"
+DOCKER_IMAGE="smart-detection_image"
+CONTAINER_NAME="smart-detection_container"
 DOCKERFILE_PATH="Dockerfile"
 
 # 인자 파싱
@@ -61,7 +61,7 @@ fi
 echo "[INFO] Running container: $CONTAINER_NAME"
 docker run -d --gpus all \
   $VOLUME_FLAGS \
-  --shm-size=32g \
+  --shm-size=64g \
   --name "$CONTAINER_NAME" \
   -it "$DOCKER_IMAGE" \
   /bin/bash
